cb6a4f5edb596dc015134d9a9c430f02da227778
bastienchabal <chabal.bastien@gmail.com>
2017-03-08 16:33:08 +0100

    Version fonctionnelle
    Cependant elle ne correspond pas à l’attente du prof étant donnée que
    l’on n’utilise pas hashchange et window.location

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-var req = new XMLHttpRequest();
-req.open("GET", url);
-req.onerror = function() {
-    console.log("Échec de chargement "+url);
-};
-req.onload = function() {
-    if (req.status === 200) {
-      var data = JSON.parse(req.responseText);
-      // do what you have to do with 'data'
-    } else {
-      console.log("Erreur " + req.status);
+window.onload = function(){
+  document.getElementById('chapitre').addEventListener('click', function(e){
+	 str = e.target.name;
+   num="";
+   for(i=1; i<str.length; i++){
+     num = num + str[i];
+   }
+   chap = "chapitre"+num+".json";
+   readTextFile(chap,  function(text){
+       var data = JSON.parse(text);
+       document.getElementById("chapitre").innerHTML = data.txt + "<br><br><ul>";
+       for(i = 0; i<data.links.length; i++){
+         document.getElementById("chapitre").innerHTML += "<li><label>"+data.links[i].txt+"</label><input name='"+ data.links[i].link +"'type='button' id='lienChap' value='"+data.links[i].link+"'></input></li>";
+       }
+       document.getElementById("chapitre").innerHTML += "</ul>";
+       //console.log(data);
+   });
+}, true);
+
+}
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
     }
-};
-req.send();
+    rawFile.send(null);
+}
+
+
+readTextFile("chapitre1.json", function(text){
+    var data = JSON.parse(text);
+    document.getElementById("chapitre").innerHTML = data.txt + "<br><br><ul>";
+    for(i = 0; i<data.links.length; i++){
+      document.getElementById("chapitre").innerHTML += "<li><label>"+data.links[i].txt+"</label><input name='"+ data.links[i].link +"'type='button' id='lienChap' value='"+data.links[i].link+"'></input></li>";
+    }
+    document.getElementById("chapitre").innerHTML += "</ul>";
+    //console.log(data);
+});
